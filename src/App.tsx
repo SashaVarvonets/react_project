b21e9df69103229cd8f84201d7ca25a662260ed5
Varvonets, Oleksandr (Contractor) <Oleksandr_Varvonets@comcast.com>
2024-07-21 12:19:00 +0300

    Intro of 'ListGroup' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import Message from "./Message.tsx";
+import ListGroup from "./components/ListGroup.tsx";
+
 function App() {
-    return <div><Message></Message></div>
+    let items = [
+        "New York",
+        "Odesa",
+        "Brasov",
+    ]
+
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+
+    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
 }
 
 export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+
+interface Props {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading , onSelectItem}: Props): JSX.Element {
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            {items.length === 0 && <p>No item</p>}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        onClick={() => {
+                            setSelectedIndex(index);
+                            onSelectItem(item);
+                        }}
+                        className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                        key={item}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
